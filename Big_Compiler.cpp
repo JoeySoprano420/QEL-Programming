e0bb8cd7466c225d753be101910eac3ac0558c8a
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-03 15:06:14 -0500

    Update Big_Compiler.cpp

--- Big_Compiler.cpp
+++ Big_Compiler.cpp
@@ -1827,3 +1827,88 @@ llvm::Value* generateCode(ExprNode* node, llvm::IRBuilder<>& builder) {
     return nullptr;
 }
 
+function_declaration:
+    FUNCTION IDENTIFIER LPAREN parameters RPAREN block {
+        $$ = new FunctionNode($2, $4, $6);
+    }
+;
+
+parameters:
+    parameter
+    | parameters COMMA parameter
+;
+
+parameter:
+    IDENTIFIER {
+        $$ = new ParameterNode($1);
+    }
+;
+
+struct ASTNode {
+    virtual ~ASTNode() = default;
+};
+
+struct NumberNode : public ASTNode {
+    int value;
+    NumberNode(int value) : value(value) {}
+};
+
+struct BinaryOpNode : public ASTNode {
+    char op;
+    ASTNode* left;
+    ASTNode* right;
+
+    BinaryOpNode(char op, ASTNode* left, ASTNode* right) : op(op), left(left), right(right) {}
+};
+
+class Parser {
+public:
+    Parser(std::vector<Token>& tokens) : tokens(tokens), position(0) {}
+
+    ASTNode* parse() {
+        return parseExpression();
+    }
+
+private:
+    std::vector<Token>& tokens;
+    size_t position;
+
+    ASTNode* parseExpression() {
+        ASTNode* left = parseTerm();
+
+        while (currentToken().type == TokenType::Operator && (currentToken().value == "+" || currentToken().value == "-")) {
+            char op = currentToken().value[0];
+            position++;
+            ASTNode* right = parseTerm();
+            left = new BinaryOpNode(op, left, right);
+        }
+        return left;
+    }
+
+    ASTNode* parseTerm() {
+        ASTNode* left = parseFactor();
+
+        while (currentToken().type == TokenType::Operator && (currentToken().value == "*" || currentToken().value == "/")) {
+            char op = currentToken().value[0];
+            position++;
+            ASTNode* right = parseFactor();
+            left = new BinaryOpNode(op, left, right);
+        }
+        return left;
+    }
+
+    ASTNode* parseFactor() {
+        if (currentToken().type == TokenType::Number) {
+            int value = std::stoi(currentToken().value);
+            position++;
+            return new NumberNode(value);
+        }
+        // Implement parsing of other constructs (e.g., parentheses, variables)
+        return nullptr;
+    }
+
+    Token currentToken() {
+        return tokens[position];
+    }
+};
+
