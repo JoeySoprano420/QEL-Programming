4830ec079c07d0cc1d533801a2b1bf16d9fa4c97
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-03 15:07:36 -0500

    Update Big_Compiler.cpp

--- Big_Compiler.cpp
+++ Big_Compiler.cpp
@@ -1969,3 +1969,29 @@ void execute(ExprNode* node) {
     }
 }
 
+class SymbolTable {
+public:
+    std::map<std::string, std::shared_ptr<ExprNode>> variables;
+    std::map<std::string, std::shared_ptr<FunctionNode>> functions;
+
+    void addVariable(const std::string& name, std::shared_ptr<ExprNode> value) {
+        variables[name] = value;
+    }
+
+    std::shared_ptr<ExprNode> getVariable(const std::string& name) {
+        if (variables.find(name) != variables.end())
+            return variables[name];
+        return nullptr; // Variable not found
+    }
+
+    void addFunction(const std::string& name, std::shared_ptr<FunctionNode> func) {
+        functions[name] = func;
+    }
+
+    std::shared_ptr<FunctionNode> getFunction(const std::string& name) {
+        if (functions.find(name) != functions.end())
+            return functions[name];
+        return nullptr; // Function not found
+    }
+};
+
